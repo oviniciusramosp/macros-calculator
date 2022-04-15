e91962128bbfb850bbff98b8604b7315a207d06f
Vinicius Ramos <viniciusarthur.rp@gmail.com>
2022-04-15 00:57:06 -0300

    adjusted steps 1 and 2 to work with navigation

--- app/screens/Step1.js
+++ app/screens/Step1.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Alert } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  Alert,
+} from "react-native";
 
 import Card from "../components/Card";
 import Toggle from "../components/ToggleItem";
@@ -7,12 +14,23 @@ import TextInputCustom from "../components/TextInputCustom";
 import Header from "../components/Header";
 import FabButtonCustom from "../components/FabButtonCustom";
 import colors from "../config/colors";
+import { StatusBar } from "expo-status-bar";
 
-function Step1(props) {
+function Step1({ navigation }) {
   const [gender, setGender] = useState("none");
   const [height, setHeight] = useState(0);
   const [weight, setWeight] = useState(0);
   const [age, setAge] = useState(0);
+
+  const maleTMB = Math.round(
+    66.5 + 13.75 * weight + 5.003 * height - 6.75 * age
+  );
+
+  const femaleTMB = Math.round(
+    655.1 + 9.563 * weight + 1.85 * height - 4.676 * age
+  );
+
+  var tMB = 0;
 
   const setToggleGender = function (genderValue) {
     if (gender === genderValue) {
@@ -28,121 +46,138 @@ function Step1(props) {
     }
     if (height > 0 && weight > 0 && age > 0) {
       if (gender === "male") {
-        return (
-          Math.round(
-            66.5 + 13.75 * weight + 5.003 * height - 6.75 * age
-          ).toString() + " Kcal"
-        );
+        tMB = maleTMB;
+        return maleTMB.toString() + " Kcal";
       } else {
-        return (
-          Math.round(
-            655.1 + 9.563 * weight + 1.85 * height - 4.676 * age
-          ).toString() + " Kcal"
-        );
+        tMB = femaleTMB;
+        return femaleTMB.toString() + " Kcal";
       }
     }
+    tMB = 0;
     return "Preencha os Campos";
   }
 
-  function showAlert(content) {
+  function showAlert(content, navigation) {
     Alert.alert(content);
   }
 
   return (
-    <View>
-      {/* Gender */}
-      <Card>
-        <Header>Selecione seu Gênero</Header>
-        <View style={styles.toggles}>
-          <Toggle
-            onPress={() => setToggleGender("male")}
-            isEmoji={true}
-            isSelected={gender === "male" ? true : false}
-          >
-            🧔‍♂️
-          </Toggle>
-          <View style={styles.margin} />
-          <Toggle
-            onPress={() => setToggleGender("female")}
-            isEmoji={true}
-            isSelected={gender === "female" ? true : false}
-          >
-            👩
-          </Toggle>
-        </View>
-      </Card>
-      {/* Medidas */}
-      <Card>
-        <Header>Suas Medidas</Header>
-        <View style={styles.toggles}>
-          <TextInputCustom
-            placeholder="000"
-            keyboardType="numeric"
-            // defaultValue={height.toString()}
-            maxLength={3}
-            returnKeyType="done"
-            textAlign="center"
-            icon={require("../assets/icons/ic_height.png")}
-            onChangeText={(value) => setHeight(value)}
-            sufix="cm"
-          >
-            Altura
-          </TextInputCustom>
-          <View style={styles.margin} />
-          <TextInputCustom
-            placeholder="000"
-            // defaultValue={weight.toString()}
-            keyboardType="numeric"
-            returnKeyType="done"
-            textAlign="center"
-            maxLength={3}
-            icon={require("../assets/icons/ic_weight.png")}
-            onChangeText={(value) => setWeight(value)}
-            sufix="Kg"
-          >
-            Peso
-          </TextInputCustom>
-        </View>
-        <View style={styles.margin} />
-        <View style={styles.toggles}>
-          <TextInputCustom
-            placeholder="00"
-            // defaultValue={age.toString()}
-            keyboardType="numeric"
-            returnKeyType="done"
-            textAlign="center"
-            maxLength={3}
-            icon={require("../assets/icons/ic_cake.png")}
-            onChangeText={(value) => setAge(value)}
-            sufix="anos"
-          >
-            Idade
-          </TextInputCustom>
-          <View style={styles.margin} />
-          <View style={styles.whiteSpace} />
-        </View>
-      </Card>
-      {/* Taxa Metabólica Basal */}
-      <Card>
-        <Header style={styles.tmbHeader}>Taxa Metabólica Basal</Header>
-        <View style={styles.tbmContent}>
-          <View style={styles.tbmIcon}>
-            <Text style={styles.emojiIcon}>🔥</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.list}>
+        <View>
+          {/* Gender */}
+          <Card>
+            <Header>Selecione seu Gênero</Header>
+            <View style={styles.toggles}>
+              <Toggle
+                onPress={() => setToggleGender("male")}
+                isEmoji={true}
+                isSelected={gender === "male" ? true : false}
+              >
+                🧔‍♂️
+              </Toggle>
+              <View style={styles.margin} />
+              <Toggle
+                onPress={() => setToggleGender("female")}
+                isEmoji={true}
+                isSelected={gender === "female" ? true : false}
+              >
+                👩
+              </Toggle>
+            </View>
+          </Card>
+          {/* Medidas */}
+          <Card>
+            <Header>Suas Medidas</Header>
+            <View style={styles.toggles}>
+              <TextInputCustom
+                placeholder="000"
+                keyboardType="numeric"
+                // defaultValue={height.toString()}
+                maxLength={3}
+                returnKeyType="done"
+                textAlign="center"
+                icon={require("../assets/icons/ic_height.png")}
+                onChangeText={(value) => setHeight(value)}
+                sufix="cm"
+              >
+                Altura
+              </TextInputCustom>
+              <View style={styles.margin} />
+              <TextInputCustom
+                placeholder="000"
+                // defaultValue={weight.toString()}
+                keyboardType="numeric"
+                returnKeyType="done"
+                textAlign="center"
+                maxLength={3}
+                icon={require("../assets/icons/ic_weight.png")}
+                onChangeText={(value) => setWeight(value)}
+                sufix="Kg"
+              >
+                Peso
+              </TextInputCustom>
+            </View>
+            <View style={styles.margin} />
+            <View style={styles.toggles}>
+              <TextInputCustom
+                placeholder="00"
+                // defaultValue={age.toString()}
+                keyboardType="numeric"
+                returnKeyType="done"
+                textAlign="center"
+                maxLength={3}
+                icon={require("../assets/icons/ic_cake.png")}
+                onChangeText={(value) => setAge(value)}
+                sufix="anos"
+              >
+                Idade
+              </TextInputCustom>
+              <View style={styles.margin} />
+              <View style={styles.whiteSpace} />
+            </View>
+          </Card>
+          {/* Taxa Metabólica Basal */}
+          <Card>
+            <Header style={styles.tmbHeader}>Taxa Metabólica Basal</Header>
+            <View style={styles.tbmContent}>
+              <View style={styles.tbmIcon}>
+                <Text style={styles.emojiIcon}>🔥</Text>
+              </View>
+              <Text style={styles.tbmLabel}>{calculate()}</Text>
+            </View>
+          </Card>
+          <View style={styles.fab}>
+            <FabButtonCustom
+              disabled={true}
+              onPress={() =>
+                navigation.navigate("Step 2", {
+                  userTMB: tMB,
+                  otherParameter: "test",
+                })
+              }
+              isEmoji={true}
+            >
+              →
+            </FabButtonCustom>
           </View>
-          <Text style={styles.tbmLabel}>{calculate()}</Text>
+          <View style={styles.margin} />
         </View>
-      </Card>
-      <View style={styles.fab}>
-        <FabButtonCustom onPress={() => showAlert("Fab Click")} isEmoji={true}>
-          →
-        </FabButtonCustom>
-      </View>
-      <View style={styles.margin} />
-    </View>
+      </ScrollView>
+      <StatusBar style="dark" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.grayLight,
+    padding: 24,
+  },
+  list: {
+    padding: 24,
+  },
   toggles: {
     flex: 1,
     flexDirection: "row",
--- app/screens/Step2.js
+++ app/screens/Step2.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView, ScrollView, View, Text, StyleSheet } from "react-native";
 import Card from "../components/Card";
+import FabButtonCustom from "../components/FabButtonCustom";
 import Header from "../components/Header";
+import { StatusBar } from "expo-status-bar";
+import colors from "../config/colors";
 
-function Step2(props) {
+function Step2({ route, navigation }) {
+  const { userTMB, otherParameter } = route.params;
+
   return (
-    <View>
-      <Card>
-        <Header>Step 2</Header>
-        <Text>AAA</Text>
-      </Card>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.list}>
+        <Card>
+          <Header>Step 2</Header>
+          <Text>{userTMB}</Text>
+        </Card>
+        <FabButtonCustom onPress={() => navigation.goBack()} isEmoji={true}>
+          ←
+        </FabButtonCustom>
+      </ScrollView>
+      <StatusBar style="dark" />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.grayLight,
+    padding: 24,
+  },
+  list: {
+    padding: 24,
+  },
+});
 
 export default Step2;
