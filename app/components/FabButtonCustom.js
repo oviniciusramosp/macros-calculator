d58b78899b2e0b75881111348ebf7a5a0d5366da
Vinicius Ramos <viniciusarthur.rp@gmail.com>
2022-04-20 19:01:03 -0300

    background and size options to Fab Button

--- app/components/FabButtonCustom.js
+++ app/components/FabButtonCustom.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, TouchableHighlight, Text } from "react-native";
 import colors from "../config/colors";
 import * as Haptics from "expo-haptics";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 function FabButtonCustom({
   children,
@@ -9,26 +11,31 @@ function FabButtonCustom({
   isEmoji = false,
   buttonStyle = "filled",
   size = "default",
+  backgroundColor = "primary",
   ...otherProps
 }) {
   return (
     <TouchableHighlight
+      onPress={onPress}
+      onPressIn={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}
       style={[
         styles.button,
         size === "small" ? styles.small : null,
+        backgroundColor === "gray" ? styles.grayButton : null,
         otherProps.disabled === true
           ? styles.buttonDisabled
           : styles.buttonEnabled,
       ]}
-      onPress={onPress}
-      onPressIn={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}
-      underlayColor={colors.primaryDark}
+      underlayColor={
+        backgroundColor === "primary" ? colors.primaryDark : colors.primaryLight
+      }
       {...otherProps}
     >
       <Text
         style={[
           (isEmoji = true ? styles.buttonEmojiLabel : styles.null),
           size === "small" ? styles.smallLabel : null,
+          backgroundColor === "gray" ? styles.grayButtonLabel : null,
         ]}
       >
         {children}
@@ -45,10 +52,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 72,
+    fontSize: 22,
   },
   small: {
     height: 52,
     width: 52,
+  },
+  smallLabel: {
+    fontSize: 22,
   },
   buttonDisabled: {
     backgroundColor: colors.grayDark,
@@ -60,8 +71,12 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "600",
   },
-  smallLabel: {
-    fontSize: 22,
+  grayButton: {
+    backgroundColor: colors.grayLight,
+    color: colors.primary,
+  },
+  grayButtonLabel: {
+    color: colors.primary,
   },
 });
 
