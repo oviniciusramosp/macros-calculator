23ea139f67d9e1af033ae81a50a4b6dbd647aed7
Vinicius Ramos <viniciusarthur.rp@gmail.com>
2022-05-07 10:49:35 -0300

    removed unnecessary Text import

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import colors from "../config/colors";
 
 import TextCustom from "../components/TextCustom";
