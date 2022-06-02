5ab8c8a785d115c619a85fe3d3d91dccd9b5b08d
Vinicius Ramos <viniciusarthur.rp@gmail.com>
2022-06-02 01:28:11 -0300

    improved Header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,28 +1,53 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import colors from "../config/colors";
-
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import TextCustom from "../components/TextCustom";
+import colors from "../config/colors";
+import IconCustom from "./IconCustom";
 
-function Header({ children, style, ...otherProps }) {
+function Header({
+  children,
+  style,
+  hasButton = false,
+  buttonAction,
+  buttonIcon,
+  ...otherProps
+}) {
   return (
-    <TextCustom
-      fontWeight="Semi Bold"
-      style={[styles.header, style]}
-      {...otherProps}
-    >
-      {children}
-    </TextCustom>
+    <View style={styles.row}>
+      <TextCustom
+        fontWeight="Semi Bold"
+        style={[styles.header, style]}
+        {...otherProps}
+      >
+        {children}
+      </TextCustom>
+      {hasButton && (
+        <TouchableOpacity onPress={buttonAction} style={styles.button}>
+          <IconCustom name={buttonIcon} color={colors.grayDark} />
+        </TouchableOpacity>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    // flexDirection: "row",
+  },
   header: {
     fontSize: 24,
     marginBottom: 16,
     lineHeight: 28,
-    color: colors.black,
-     letterSpacing: -0.7,
+    letterSpacing: -0.7,
+  },
+  button: {
+    height: 52,
+    width: 52,
+    padding: 14,
+    position: "absolute",
+    right: -14,
+    top: -12,
   },
 });
 
